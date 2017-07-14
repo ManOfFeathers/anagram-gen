ddf1b84590d021e46500f24892295a6466da11c6
ManOfFeathers <griffintmyers@gmail.com>
2017-07-13 20:02:25 -0600

    Removed redundant code

--- anagram.py
+++ anagram.py
@@ -1,17 +1,13 @@
-from random import *
+import random
 
 def anagram(name):
     s = name    # converts input to string
     b = len(s)  # b for blank spaces
 ##    r = b - 1   # range of numerical values assigned to characters
     a = []      # array to be filled by rearranged letters
-##    r = Random()
-##    r2 = int(r * 100) // b 
     for i in range (b):
-        if i <= b:
-            a.append(i)
-    
-
+        ##r = random.randint(0,b)
+        a.append(i)
     return(a)
 
-print(anagram("abraham lincoln"))
+print(anagram("thomas"))
