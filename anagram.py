01f4c6307312c1f506082475c696d92fd104943f
ManOfFeathers <griffintmyers@gmail.com>
2017-07-13 21:05:03 -0600

    Commented unfinished code

--- anagram.py
+++ anagram.py
@@ -4,16 +4,16 @@ def anagram(name):
     s = name    # converts input to string
     b = len(s)  # b for blank spaces
     a = []      # array to be filled by rearranged letters
-    a2 = []
+##    a2 = []
     for i in range(b):
         a.append(i)
 
-    r = random.randint(0,b)
-    
-    for i in range(b):
-        np = a[r]
-        a2.append(np)
+##    r = random.randint(0,b)
+##    
+##    for i in range(b):
+##        np = a[r]
+##        a2.append(np)
 
         
-    return(a, a2)
+    return(a)
 print(anagram("tartuffe"))
