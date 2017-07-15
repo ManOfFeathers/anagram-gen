145fdfdc4ce694d0b0c0c0aca91153c14ed247a0
ManOfFeathers <griffintmyers@gmail.com>
2017-07-15 00:25:07 -0600

    Added new function

--- anagram.py
+++ anagram.py
@@ -1,17 +1,29 @@
-##import random
+import random
 
-def anagram(name):
+def char_array(name):
     s = name    # converts input to string
     b = len(s)  # b for blank spaces
-    a = []      # array to be filled by rearranged letters
-##    a2 = []
-##    r = random.randint(0,b)
-##    
-##    for i in range(b):
-##        np = a[r]
-##        a2.append(np)
+    a = []      # array to be filled by rearranged letters 
+
     for i in range(b):
         a.append(i)
-
+    
     return(a)
+print(char_array("tartuffe"))
+
+def anagram(name):
+    s = name    
+    b = len(s)
+
+    a2 = char_array(s)
+
+    for i in range(b): 
+        r = random.randint(0,b)
+        a2[i] = r
+        if (a2[i-1] == r):
+           r = random.randint(0,b)
+           a2[i] = r
+        a2.append(r)
+    return(a2)
+
 print(anagram("tartuffe"))
