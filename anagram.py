fd9d7380353a5008d477fa5a022f5838df5b25c6
ManOfFeathers <griffintmyers@gmail.com>
2017-07-23 19:13:16 -0600

    Created stable function for one word

--- anagram.py
+++ anagram.py
@@ -1,6 +1,13 @@
+from random import *
+from collections import defaultdict
 import random
 import urllib.request
-from collections import defaultdict
+
+ws = urllib.request.urlopen('http://www.puzzlers.org/pub/wordlists/unixdict.txt').read().split()
+                            # https://raw.githubusercontent.com/dwyl/english-words/master/words.txt
+                            # http://www.puzzlers.org/pub/wordlists/unixdict.txt
+                            # http://www-01.sil.org/linguistics/wordlists/english/
+                            # ws for words, put each word on its own line
 
 def char_array(name):
     s = name                # converts input to string
@@ -9,14 +16,24 @@ def char_array(name):
     random.shuffle(a)       # mix up array letters
     return "".join(a)       # generate results as a string
 
+def scramble_checker(sentence, min_len):
+    gl = sentence.split(" ") # gibberish list
+    nl = []
+    for word in gl:
+        ana = anagram(word)
+        if ana:
+            aw = [] # accepted words
+            for word in ana:
+                if len(word) > min_len:
+                    aw.append(word)
+            if aw:
+                choice = random.choice(aw)
+                nl.append(choice)
+    return nl
+
 # function coded with help from https://rosettacode.org/wiki/Anagrams#Python
 def anagram(name):
     s = name
-    ws = urllib.request.urlopen('http://www.puzzlers.org/pub/wordlists/unixdict.txt').read().split()
-                            # https://raw.githubusercontent.com/dwyl/english-words/master/words.txt
-                            # http://www.puzzlers.org/pub/wordlists/unixdict.txt
-                            # http://www-01.sil.org/linguistics/wordlists/english/
-                            # ws for words, put each word on its own line
     ana = defaultdict(list)
     gram = None             # an empty anagram-related variable
 
@@ -41,34 +58,30 @@ def new_anagram(sentence):
             nl.append(w)
     return " ".join(nl)
 
-def scramble_checker(sentence, min_len):
-    gl = sentence.split(" ") # gibberish list
-    nl = []
-    for word in gl:
-        ana = anagram(word)
-        if ana:
-            aw = [] # accepted words
-            for word in ana:
-                if len(word) > min_len:
-                    aw.append(word)
-            if aw:
-                choice = random.choice(aw)
-                nl.append(choice)
-    return nl
-
 # print(scramble_checker(char_array("erasmus tied cartesian silt citrus"))) # used for testing
 
 def best_anagram(string, min_len):
     nl = []
     i = 0
     while not nl: # while new list is empty
-        print(i)
+        print(i) # testing purposes -- shows us that it's thinking
         g = char_array(string) # g for gibberish
         nl = scramble_checker(g, min_len)
         i += 1
     return " ".join(nl), i
 ##    return(nl)
 
-print(best_anagram("erasmus tied cartesian silt citrus", 4)) # used for testing
+def smart_anagram(text, maxiter):
+    wl = [] # word list
+    il = [] # iteration list`
+    min_len = 3 # minimum word length
+    (output, iters) = best_anagram(text, min_len)
+    for i in range(maxiter):
+        min_len = len(output) + 1
+        wl.append(output)
+        il.append(iters)
+    return wl, il
 
-# erasmus tied cartesian silt citrus
+print(smart_anagram("erasmus tied cartesian silt citrus", 3))
+
+# print((output, iters)) # used for testing
