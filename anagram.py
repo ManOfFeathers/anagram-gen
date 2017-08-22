71388f80f462986e64d92e4589b5356ac74d5154
ManOfFeathers <griffintmyers@gmail.com>
2017-08-22 11:16:34 -0600

    Two words!

--- anagram.py
+++ anagram.py
@@ -8,7 +8,7 @@ from collections import defaultdict
 import random
 import urllib.request
 
-ws = urllib.request.urlopen('http://www-01.sil.org/linguistics/wordlists/english/wordlist/wordsEn.txt').read().split()
+ws = urllib.request.urlopen('http://www.puzzlers.org/pub/wordlists/unixdict.txt').read().split()
                             # https://raw.githubusercontent.com/dwyl/english-words/master/words.txt
                             # http://www.puzzlers.org/pub/wordlists/unixdict.txt
                             # http://www-01.sil.org/linguistics/wordlists/english/wordlist/wordsEn.txt
@@ -84,16 +84,21 @@ def best_anagram(string, min_len):
 #     return wl, il
 
 def smart_anagram(string, min_len):
+    ol = string.split(" ") #old list
     nl = []
     i = 0
-    while not nl: # while new list is empty
-        print(i) # testing purposes -- shows us that it's thinking
-        nl = scramble_checker(char_array(string), min_len) # nl.append(scramble_checker(g, min_len))
-        i += 1
+    #print(ol)
+    for word in ol:
+        while not nl: # while new list is empty
+            print(i) # testing purposes -- shows us that it's thinking
+            nl = scramble_checker(char_array(word), min_len)  # nl.append(scramble_checker(g, min_len))
+            #del ol[i]
+            if nl:
+                i += 1
     return nl, i
 
+print(best_anagram("erasmus tied cartesian silt citrus", 3)) # used for testing
 
-print(smart_anagram("erasmus tied cartesian silt citrus", 3)) # used for testing
 # print(scramble_checker("erasmus tied cartesian silt citrus", 3))
 # print(scramble_checker(char_array("erasmus tied cartesian silt citrus"), 3)) # used for testing
 
